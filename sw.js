5a232b7aa36dcb9390bed500f8611da66c17108f
xblspain-glitch <xblspain@gmail.com>
2026-06-20 22:08:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="oraciones-versiculos-v8_2_fix_categoria";
+const CACHE_NAME="oraciones-v2-fase-oraciones-modernas-fix1";
 const CORE=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js"];
 self.addEventListener("install",event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));const cache=await caches.open(CACHE_NAME);await cache.addAll(CORE);})());self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));await self.clients.claim();})());});
